455ef962ae93ea4033616c4124e99a3de2d59f23
Engr Thankgod <gbuzueejiketh@gmail.com>
2026-09-23 09:36:57 +0100

    Security Hardening Sec003

--- backend/src/modules/employee/employee.module.ts
+++ backend/src/modules/employee/employee.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../core/prisma/prisma.module';
 import { AuditModule } from '../audit/audit.module';
-import { EmployeeController } from './employee.controller';
 import { EmployeeImportReportService } from './employee-import-report.service';
 import { EmployeeImportService } from './employee-import.service';
 import { EmployeeService } from './employee.service';
 
 @Module({
   imports: [PrismaModule, AuditModule],
-  controllers: [EmployeeController],
   providers: [
     EmployeeService,
     EmployeeImportService,
